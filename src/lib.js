634786c8330b0c86f28a6b3169a519420cee4f38
gabrielchase <gchase.patron@gmail.com>
2018-12-31 23:02:13 -0700

    Function for headers with JWT

---
+++ src/lib.js
@@ -0,0 +1,10 @@
+module.exports = {
+    getHeadersWithJWT: () => {
+        const token = localStorage.getItem('token')
+        return {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }
+    }
+}
