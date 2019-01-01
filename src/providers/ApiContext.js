d065ecaea3d19960bf467bd7c8ffebe731c268d0
gabrielchase <gchase.patron@gmail.com>
2018-12-31 23:02:42 -0700

    Get payments from API Provider

--- src/providers/ApiContext.js
+++ src/providers/ApiContext.js
@@ -3,6 +3,8 @@ import axios from 'axios'
 
 import { API_URL } from '../constants.json'
 
+import lib from '../lib'
+
 const ApiContext = React.createContext()
 
 class ApiProvider extends Component {
@@ -10,6 +12,25 @@ class ApiProvider extends Component {
         is_auth: false,
         payments: [],
         order: {}
+    }
+
+    getPayments = async (year, month, date) => {
+        const _year = parseInt(year)
+        const _month = parseInt(month)
+        const _date = parseInt(date)
+        console.log('date: ', date)
+        console.log(`${API_URL}/payments?year=${_year}&month=${_month}&date=${_date}`)
+        const headers = lib.getHeadersWithJWT()
+        const payments_this_month = await axios.get(`${API_URL}/payments?year=${_year}&month=${_month}`, headers)
+        const payments_today = await axios.get(`${API_URL}/payments?year=${_year}&month=${_month}&date=${_date}`, headers)
+
+        console.log('this_month: ', payments_this_month.data)
+        console.log('today: ', payments_today.data)
+        
+        if (payments_this_month.data.success && payments_today.data.success)
+            return { payments_this_month: payments_this_month.data, payments_today: payments_today.data }
+        else 
+            return []
     }
 
     login = async (farm_name, password) => {
@@ -30,8 +51,13 @@ class ApiProvider extends Component {
 
     render() {
         return (
-            <ApiContext.Provider value={{ is_auth: this.state.is_auth, login: this.login, logout: this.logout }}>
-                {this.props.children}
+            <ApiContext.Provider value={{ 
+                    is_auth: this.state.is_auth, 
+                    login: this.login, 
+                    logout: this.logout,
+                    getPayments: this.getPayments
+                    }}>
+                        {this.props.children}
             </ApiContext.Provider>
         )
     }
