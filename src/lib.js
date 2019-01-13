8a681b16c3b7c11ad6090ad585c9ff8e8792ff8e
gabrielchase <gchase.patron@gmail.com>
2019-01-13 20:42:33 +0800

    display Date and money functions

--- src/lib.js
+++ src/lib.js
@@ -1,3 +1,5 @@
+const { MONTHS } = require('./constants')
+
 module.exports = {
     getHeadersWithJWT: () => {
         const token = localStorage.getItem('token')
@@ -6,5 +8,15 @@ module.exports = {
                 Authorization: `Bearer ${token}`
             }
         }
+    },
+    displayDate: (d) => {
+        const date_string = d.split('T')[0]
+        const year = date_string.split('-')[0]
+        const month = date_string.split('-')[1]
+        const date = date_string.split('-')[2]
+        return `${MONTHS[month-1]} ${date}, ${year}`
+    },
+    displayMoney: (m) => {
+        return 'P' + m.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     }
 }
